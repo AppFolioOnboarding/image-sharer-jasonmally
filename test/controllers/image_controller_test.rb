58c95a1c8440c1bba93fcc90364bac375219bc20
Jason <jason.mally@appfolio.com>
2019-06-28 13:54:47 -0700

    Seeded image DB, added controller tests.

--- test/controllers/image_controller_test.rb
+++ test/controllers/image_controller_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class ImageControllerTest < ActionDispatch::IntegrationTest
-  # test "should get home" do
-  #   get image_home_url
-  #   assert_response :success
-  # end
+  setup do
+    @image = Image.create!(name: "Test Image", image_url: "https://pixy.org/src/456/4564782.jpeg")
+  end
+
+  def test_home
+    get root_path
+    assert_response :ok
+    assert_select '#header', 'All Saved Images'
+  end
+
+  def test_form
+    get new_image_path
+    assert_response :ok
+    assert_select '#header', 'Save Image'
+  end
+
+  def test_create_success
+    assert_difference('Image.count', 1) do
+      image_params =  { name: 'Second Test Image', image_url: "https://i.imgur.com/CAujwlD.jpg"}
+      post image_index_path, params: { image: image_params}
+    end
+    assert_redirected_to image_path(Image.last)
+  end
+
+  def test_create_fail
+    assert_no_difference('Image.count') do
+      image_params =  { name: 'Second Test Image', image_url: "hts://i.imgur.com/CAujwlD.jpg"}
+      post image_index_path, params: { image: image_params}
+    end
+    assert_response :unprocessable_entity
+  end
+
 end
