e05875f143b081190a1e478d6704ce693d997782
Jason <jason.mally@appfolio.com>
2019-06-28 14:09:39 -0700

    Made image model tests more robust

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,27 +1,34 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  test 'Image 1. An image with no text or URL should not be accepted' do
+  def test_empty_image_params
     img = Image.new
     assert_not img.valid?
+    assert_equal 'Please enter a title for your image.', img.errors.messages[:name].first
+    assert_includes img.errors.messages[:image_url], 'Please enter a URL for your image.'
+    assert_includes img.errors.messages[:image_url], 'This image URL is invalid.'
   end
 
-  test 'Image 2.1. An image missing a field should not be accepted' do
+  def test_empty_image_url
     img = Image.new(name: "woo this isn't real")
     assert_not img.valid?
+    assert_includes img.errors.messages[:image_url], 'Please enter a URL for your image.'
+    assert_includes img.errors.messages[:image_url], 'This image URL is invalid.'
   end
 
-  test 'Image 2.2. An image missing a field should not be accepted' do
+  def test_empty_image_name
     img = Image.new(image_url: 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg')
     assert_not img.valid?
+    assert_equal 'Please enter a title for your image.', img.errors.messages[:name].first
   end
 
-  test 'Image 3. An image with an invalid URL should not be accepted' do
+  def test_image_invalid_url
     img = Image.new(name: 'This is a title', image_url: 'htp:/www.wrong.com')
     assert_not img.valid?
+    assert_equal 'This image URL is invalid.', img.errors.messages[:image_url].first
   end
 
-  test 'Image 4. An image with a title and a valid URL should be accepted' do
+  def test_valid_image
     img = Image.new(name: 'Finally!', image_url: 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg')
     assert img.valid?
   end
